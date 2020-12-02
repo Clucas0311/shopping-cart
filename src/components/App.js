92e46e85060109f45bbf919b84f8ec99e80af2b5
Clucas0311 <clucas0311@gmail.com>
2020-12-02 15:31:14 -0500

    Fixed component bug

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Todos from './Todos';
 
 class App extends React.Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Take out the trash',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'Dinner with wife',
+				completed: false
+			},
+			{
+				id: 3,
+				title: 'Meeting with boss',
+				completed: false
+			}
+		]
+	};
 	render() {
-		return <div>App</div>;
+		console.log(this.state.todos);
+		return (
+			<div className="App">
+				<Todos />
+			</div>
+		);
 	}
 }
 
