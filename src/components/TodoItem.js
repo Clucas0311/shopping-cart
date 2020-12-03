4bdfd2f75d650ad7cdebf122ca389e672965fc32
Clucas0311 <clucas0311@gmail.com>
2020-12-02 20:51:05 -0500

    Adding the prototype from todoitem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.Component {
 	render() {
 		return <div>{this.props.todo.title}</div>;
 	}
 }
+TodoItem.propTypes = {
+	todo: PropTypes.object.isRequired
+};
 
 export default TodoItem;
