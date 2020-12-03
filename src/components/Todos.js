c13aab9fc009ec67972cc261ecc81d62d4c9b50c
Clucas0311 <clucas0311@gmail.com>
2020-12-02 20:51:37 -0500

    Fixed bug

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
+
 class Todos extends React.Component {
 	render() {
 		return this.props.todos.map((todo) => {
@@ -8,8 +9,5 @@ class Todos extends React.Component {
 		});
 	}
 }
-Todos.propTypes = {
-	todos: PropTypes.array.isRequired
-};
 
 export default Todos;
