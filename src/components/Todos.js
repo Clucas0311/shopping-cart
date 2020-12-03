7da59a0437f8ea5ffc04946c0220bced14db1e11
Clucas0311 <clucas0311@gmail.com>
2020-12-02 20:49:22 -0500

    Adding PropTypes

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 class Todos extends React.Component {
 	render() {
 		return this.props.todos.map((todo) => {
@@ -7,5 +8,8 @@ class Todos extends React.Component {
 		});
 	}
 }
+Todos.propTypes = {
+	todos: PropTypes.array.isRequired
+};
 
 export default Todos;
